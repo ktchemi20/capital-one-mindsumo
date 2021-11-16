1b48db458535ea0377219b9edbc0174e4306e092
ktchemi20@students.desu.edu <ktchemi20@students.desu.edu>
2021-11-15 23:24:03 -0500

    final draft

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import './Header.css';
+import './Header.css'
 
 const Header = () => {  
     return (
-        <div>
+        <div className= "header">
             <h1>National Park Service</h1>
             <p> Looking for your next adventure?</p>
         </div>
